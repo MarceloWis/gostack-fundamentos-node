c0945112f23b5096c592efcf185a2a5bdd015e17
Marcelo Wischniowski <marcelowisc@gmail.com>
2020-04-15 21:03:44 -0300

    fundamentos go node

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,34 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    const transactions = this.transactions;
+    return transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const transactions = this.transactions;
+
+    const income = transactions
+      .filter(transactions => transactions.type === 'income')
+      .reduce((incomeSum, currentValue) => incomeSum + currentValue.value, 0);
+    const outcome = transactions
+      .filter(transactions => transactions.type === 'outcome')
+      .reduce((outcomeSum, currentValue) => outcomeSum + currentValue.value, 0);
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
